f34a66efd01a886f6aabb2708caaa8dada3bf0db
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2017-10-12 14:49:48 +0700

    Update Shape.php

--- Shape.php
+++ Shape.php
@@ -1,12 +1,22 @@
 <?php
 
-//kelas abstrak dari bentuk
+/**
+ * kelas abstrak dari bentuk
+ */
 abstract class Shape
 {
-	// macam bentuknya dan nilainya (value) dari lebar dan tinggi
+    /**
+     * property lebar
+     */
     protected $width = 0;
+
+    /**
+     * property tinggi
+     */
     protected $height = 0;
     
-    //fungsi abstrak untuk perintah menghitung luasnya
+    /**
+     * fungsi abstrak untuk perintah menghitung luasnya
+     */
     abstract public function getArea();
 }
