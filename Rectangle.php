ff6f7b8d50fe92ddcb6e40be59ba815d9c1c8d13
Trisantiana <trisantiana9@gmail.com>
2017-10-12 14:20:06 +0700

    #9 dekripsi property

--- Rectangle.php
+++ Rectangle.php
@@ -1,19 +1,22 @@
 <?php
-
+// membuat kelas segiempat
 class Rectangle extends Shape
 {
+    // fungsi umum yang menetapkan nilai lebar 
     public function setWidht(int $width)
     {
         $this->width = $width;
     }
-
+    // fungsi umum yang menetapkan nilai tingginya
     public function setHeight(int $height)
     {
         $this->height = $height;
     }
-
+    
+    // fungsi umum untuk menghitung area/luasnya
     public function getArea(): int
     {
+        // perintah/cara menghitungnya (lebar * tinggi)
         return $this->width * $this->height;
     }
 }
--- Shape.php
+++ Shape.php
@@ -1,9 +1,12 @@
 <?php
 
+//kelas abstrak dari bentuk
 abstract class Shape
 {
+	// macam bentuknya dan nilainya (value) dari lebar dan tinggi
     protected $width = 0;
     protected $height = 0;
-
+    
+    //fungsi abstrak untuk perintah menghitung luasnya
     abstract public function getArea();
 }
